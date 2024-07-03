73f8d76d75dd80e7204feac4d3c65afd58bd60ad
Hrigeo <g.achkov@yahoo.com>
2024-07-03 18:24:22 +0300

    Hide the Connection String

--- ReceptionProject/Program.cs
+++ ReceptionProject/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ReceptionProject.Data;
@@ -36,9 +37,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 app.Run();
